10ab85acb48ceba537d7de50c8b09d65c5ee0259
qq52389433 <52389433@qq.com>
2019-02-05 10:28:34 +0800

    update earthcoin

---
+++ p2pool/bitcoin/networks/earthcoin.py
@@ -0,0 +1,28 @@
+import os
+import platform
+
+from twisted.internet import defer
+
+from .. import data, helper
+from p2pool.util import pack
+
+
+P2P_PREFIX = 'c0dbf1fd'.decode('hex')
+P2P_PORT = 35677
+ADDRESS_VERSION = 0
+RPC_PORT = 8332
+RPC_CHECK = defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
+            (yield helper.check_genesis_block(bitcoind, '0007e5a233e96f7b8d2413060ec38cf73c6f201bdb72f97b3241cc8ac6950a81')) and
+            (yield bitcoind.rpc_getblockchaininfo())['chain'] != 'test'
+        ))
+SUBSIDY_FUNC = lambda height: 50*100000000 >> (height + 1)//210000
+POW_FUNC = data.hash256
+BLOCK_PERIOD = 600 # s
+SYMBOL = 'EAC'
+CONF_FILE_FUNC = lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Bitcoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Bitcoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.bitcoin'), 'bitcoin.conf')
+BLOCK_EXPLORER_URL_PREFIX = 'https://blockchain.info/block/'
+ADDRESS_EXPLORER_URL_PREFIX = 'https://blockchain.info/address/'
+TX_EXPLORER_URL_PREFIX = 'https://blockchain.info/tx/'
+SANE_TARGET_RANGE = (2**256//2**32//1000000 - 1, 2**256//2**32 - 1)
+DUMB_SCRYPT_DIFF = 1
+DUST_THRESHOLD = 0.001e8
