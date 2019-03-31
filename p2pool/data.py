9c6ca99e0ebc71408bc357e210153aac2498a609
qq52389433 <52389433@qq.com>
2019-03-31 11:02:48 +0800

    update node

--- p2pool/data.py
+++ p2pool/data.py
@@ -506,6 +506,7 @@ class OkayTracker(forest.Tracker):
             return True
     
     def think(self, block_rel_height_func, previous_block, bits, known_txs):
+        print 'init best_share think!'
         desired = set()
         bad_peer_addresses = set()
         
@@ -578,6 +579,7 @@ class OkayTracker(forest.Tracker):
             -self.items[h].should_punish_reason(previous_block, bits, self, known_txs)[0],
             -self.items[h].time_seen,
         ), h) for h in self.verified.tails.get(best_tail, []))
+        print 'init think get_best!'
         if p2pool.DEBUG:
             print len(decorated_heads), 'heads. Top 10:'
             for score, head_hash in decorated_heads[-10:]:
